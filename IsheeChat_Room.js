15687efcac3f98a66cface3cd311bc0c840e887b
ishee25 <74249367+ishee25@users.noreply.github.com>
2021-01-19 19:32:50 +0530

    Add files via upload

---
+++ IsheeChat_Room.js
@@ -0,0 +1,13 @@
+// ADD FIREBASE LINKS HERE
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCghOO7INYeT8CIYwQLItpjcswlebXyevE",
+    authDomain: "isheechat-web-app.firebaseapp.com",
+    databaseURL: "https://isheechat-web-app-default-rtdb.firebaseio.com",
+    projectId: "isheechat-web-app",
+    storageBucket: "isheechat-web-app.appspot.com",
+    messagingSenderId: "711410488553",
+    appId: "1:711410488553:web:69aee7d3d1adc5cf7d0c78"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
